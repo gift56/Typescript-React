6920057d9d94466f4a002f2b5a3870de1af493f3
gift56 <efeasiughu@gmail.com>
2022-09-27 14:21:13 +0100

    working on home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
 
 function App() {
-  return <div className="container">hello world</div>;
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
