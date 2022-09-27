1f7902a869a32fdf552c2bf03c3ae83b2dac8f67
gift56 <efeasiughu@gmail.com>
2022-09-27 14:51:44 +0100

    fixed one issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,24 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [active, setActive] = useState(0);
+
+  const Links = [
+    { link: "", name: "home" },
+    { link: "about", name: "about" },
+    { link: "service", name: "service" },
+  ];
   return (
     <header className="header">
       <div className="container">
         <nav className="nav">
           <ul className="navLinks">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/store">Store</Link>
-            </li>
+            {Links.map(({ link, name }) => (
+              <li key={name}>
+                <Link to={`/${link}`}>{name}</Link>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
