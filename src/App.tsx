6c87193df9a403472035c8d6c86c1497ce395f69
gift56 <efeasiughu@gmail.com>
2022-09-27 14:26:20 +0100

    imported navbar in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 
 function App() {
   return (
-    <div className="container mb-4">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+    <>
+      <Navbar />
+      <div className="container mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
