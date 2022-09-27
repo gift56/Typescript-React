590972183fd47c8860ebe7d8724c40f1869104fd
gift56 <efeasiughu@gmail.com>
2022-09-27 14:41:35 +0100

    started routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <header>
-      <div className="container">Nav</div>
+    <header className="header">
+      <div className="container">
+        <nav className="nav">
+          <ul className="navLinks">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/store">Store</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
