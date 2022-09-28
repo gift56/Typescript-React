58e623553aa1d9cf08886b123d8ddf24b3ff8502
gift56 <efeasiughu@gmail.com>
2022-09-28 14:42:37 +0100

    still fixing issues

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -9,15 +9,25 @@ type CartItemProps = {
 
 const CartItem = ({ id, quantity }: CartItemProps) => {
   const { removeFromCart } = useShoppingCart();
-  const item = storeItems.find(i => i.id === id)
+  const item = storeItems.find((i) => i.id === id);
   if (item == null) return null;
 
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack direction="horizontal" gap={2} className="d-f a-center">
       <img
         src={item.imgUrl}
         style={{ width: "125px", height: "125px", objectFit: "cover" }}
       />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: "0.65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+      </div>
     </Stack>
   );
 };
