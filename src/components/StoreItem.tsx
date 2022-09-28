70334cbccd4941784908d65418acbb56e9f8a723
gift56 <efeasiughu@gmail.com>
2022-09-28 09:14:28 +0100

    cstarted coding today

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StoreItem = () => {
+  return (
+    <div>StoreItem</div>
+  )
+}
+
+export default StoreItem
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,17 @@
+import StoreItem from "../components/StoreItem";
+import storeItems from "../data/items.json";
+
 const Store = () => {
   return (
-    <div>Store</div>
-  )
-}
+    <>
+      <h1>Store</h1>
+      <div className="grid">
+        {storeItems.map((item) => (
+          <div className="col" key={item.id}><StoreItem {...item} /></div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Store
+export default Store;
