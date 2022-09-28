7e262f3ba28a07b6e5cf39b410d84bef9408b75a
gift56 <efeasiughu@gmail.com>
2022-09-28 11:11:31 +0100

    updated some stuff

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -2,6 +2,7 @@ import { formatCurrency } from "../utilities/formatCurrency";
 
 const StoreItem = ({ item }: any) => {
   const { id, name, price, imgUrl } = item;
+  const quantity = 0;
   return (
     <div className="card">
       <div className="image">
@@ -11,6 +12,15 @@ const StoreItem = ({ item }: any) => {
         <div className="title">
           <h4>{name}</h4>
           <span>{formatCurrency(price)}</span>
+        </div>
+        <div className="btn">
+          {quantity == 0 ? (
+            <button className="btn">+ Add To Cart</button>
+          ) : (
+            <div className="addingCart">
+                
+            </div>
+          )}
         </div>
       </div>
     </div>
