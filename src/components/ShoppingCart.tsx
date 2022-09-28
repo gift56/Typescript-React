9340acc8d4bd8f186dee9403687ec5fcb63ab322
gift56 <efeasiughu@gmail.com>
2022-09-28 14:22:23 +0100

    working on cartItem

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,16 +1,24 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import CartItem from "./CartItem";
 
 type ShoppingCartProps = {
   isOpen: boolean;
 };
 
 const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
-  const { closeCart } = useShoppingCart();
+  const { closeCart, cartItems } = useShoppingCart();
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement="end">
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
+        <Offcanvas.Body>
+          <Stack gap={3}>
+            {cartItems.map((item) => (
+              <CartItem key={item.id} {...item} />
+            ))}
+          </Stack>
+        </Offcanvas.Body>
       </Offcanvas.Header>
     </Offcanvas>
   );
