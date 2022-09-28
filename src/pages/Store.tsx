75f129218f687b3d2c7cbca0798e595d74794945
gift56 <efeasiughu@gmail.com>
2022-09-28 16:04:09 +0100

    good night

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,12 @@
 import StoreItem from "../components/StoreItem";
 import storeItems from "../data/items.json";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Store = () => {
+  const [product, setProduct] = useState([]);
+  const fetchingProducts = async () => {};
+
   return (
     <>
       <h1>Store</h1>
