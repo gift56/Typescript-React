c0049e8b8f82e4e21f53af723dfc9ca2156e3baa
gift56 <efeasiughu@gmail.com>
2022-09-29 09:41:06 +0100

    removed about

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,9 +17,6 @@ const Navbar = () => {
           <Nav.Link to="/store" as={NavLink}>
             Store
           </Nav.Link>
-          <Nav.Link to="/about" as={NavLink}>
-            About
-          </Nav.Link>
         </Nav>
 
         {cartQuantity > 0 && (
