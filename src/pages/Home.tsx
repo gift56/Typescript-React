74451dfa4aef570e9abc0f341ea35e8d35385caf
gift56 <efeasiughu@gmail.com>
2022-09-29 11:16:04 +0100

    fixed typescript issues

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
+import { Products } from "../utilities/interface";
+import axios from "axios";
 
 const Home = () => {
+  const [product, setProduct] = useState<Products[]>([]);
+
+  const fetchAllProducts = async () => {
+    axios.get<Products[]>("https://fakestoreapi.com/products").then((res) => {
+      setProduct(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+  const { category, description, id, image, price, title }: any = product;
+
+  console.log(product);
+
   return (
     <div>
-        <h1>Home</h1>
+      <div className="grid">
+        <div className="card">
+          <div className="image">{/* <img src={title} alt={title} /> */}</div>
+          <div className="body">
+            <div className="title">
+              <h4>{title}</h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/utilities/interface.ts
@@ -0,0 +1,8 @@
+export interface Products {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  title: string;
+}
