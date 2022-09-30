c36e9f537e62fef7a308b5398da443d25ecadbbc
gift56 <efeasiughu@gmail.com>
2022-09-30 09:43:59 +0100

    removing some stuff

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,13 @@
-import { useContext } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { Nav, Navbar as NavbarBs } from "react-bootstrap";
-import { ThemeContext } from "../context/ThemeContext";
-import styles from "../sass/Header.module.scss";
-import { BsFillMoonFill } from "react-icons/bs";
 
 const Navbar = () => {
   const { openCart, cartQuantity } = useShoppingCart();
-  const { theme, setTheme } = useContext(ThemeContext);
 
   return (
-    <NavbarBs sticky="top" className={`shadow-sm mb-3 header ${styles[theme]}`}>
+    <NavbarBs sticky="top" className="shadow-sm mb-3 header">
       <div className="container">
         <Nav className="myLinks">
           <Nav.Link to="/" as={NavLink}>
@@ -22,20 +17,13 @@ const Navbar = () => {
             Store
           </Nav.Link>
         </Nav>
-        <div className="center">
-          <span
-            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-            className="toggler"
-          >
-            <BsFillMoonFill />
-          </span>
-          {cartQuantity > 0 && (
-            <button className="btn myBtn countes" onClick={() => openCart()}>
-              <AiOutlineShoppingCart size={30} color="#fff" />
-              <p className="numbers">{cartQuantity}</p>
-            </button>
-          )}
-        </div>
+
+        {cartQuantity > 0 && (
+          <button className="btn myBtn countes" onClick={() => openCart()}>
+            <AiOutlineShoppingCart size={30} color="#fff" />
+            <p className="numbers">{cartQuantity}</p>
+          </button>
+        )}
       </div>
     </NavbarBs>
   );
