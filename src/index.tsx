b33b3d272d8c7d0e750fca5a7d170313872e5305
gift56 <efeasiughu@gmail.com>
2022-09-30 09:44:50 +0100

    removing theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,15 @@ import "./sass/styles.scss";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ThemeProvider } from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
